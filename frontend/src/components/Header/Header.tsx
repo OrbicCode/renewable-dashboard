fc0d6a49b4a7a1cc895089913018fcb95388005c
James O'Kane <orbiccode@gmail.com>
2025-11-12 20:31:24 +0000

    added menu icon

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,8 +1,10 @@
 import styles from './Header.module.css';
+import MenuIcon from '@mui/icons-material/Menu';
 
 export default function Header() {
   return (
     <header className={styles.container}>
+      <MenuIcon fontSize="large" />
       <nav></nav>
     </header>
   );
