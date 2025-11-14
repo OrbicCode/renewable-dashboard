55bf09925490bf7d17e56ed72306c184541d76f5
James O'Kane <orbiccode@gmail.com>
2025-11-14 19:08:17 +0000

    implemented basic nav menu toggle

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,11 +1,32 @@
+import { useState } from 'react';
 import styles from './Header.module.css';
 import MenuIcon from '@mui/icons-material/Menu';
+import { Close } from '@mui/icons-material';
 
 export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+
+  function handleMenuToggle() {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
   return (
     <header className={styles.container}>
-      <MenuIcon fontSize="large" />
-      <nav></nav>
+      <button onClick={handleMenuToggle}>
+        <MenuIcon fontSize="large" />
+      </button>
+      <nav className={`${styles.nav} ${isMenuOpen ? styles.navOpen : ''}`}>
+        <button onClick={handleMenuToggle} className={styles.closeBtn}>
+          <Close fontSize="large" />
+        </button>
+        <ul>
+          <li>Live / Historical</li>
+          <li>Dashboard</li>
+          <li>About</li>
+          <li>Data Sources</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
     </header>
   );
 }
