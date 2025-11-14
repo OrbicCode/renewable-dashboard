69e44c891d0d5fea7d843ba143eeca1c8698cbac
James O'Kane <orbiccode@gmail.com>
2025-11-14 21:04:25 +0000

    added info and filter icons

--- frontend/src/components/ChartHeader/ChartHeader.tsx
+++ frontend/src/components/ChartHeader/ChartHeader.tsx
@@ -1,14 +1,17 @@
 import styles from './ChartHeader.module.css';
 import InfoOutlineIcon from '@mui/icons-material/InfoOutline';
+import TuneIcon from '@mui/icons-material/Tune';
 
 export default function ChartHeader() {
   return (
     <div className={styles.container}>
       <h2 style={{ margin: '0' }}>Title</h2>
       <div>
-        <button>Filters</button>
         <button>
-          <InfoOutlineIcon />
+          <TuneIcon fontSize="small" />
+        </button>
+        <button>
+          <InfoOutlineIcon fontSize="small" />
         </button>
       </div>
     </div>
