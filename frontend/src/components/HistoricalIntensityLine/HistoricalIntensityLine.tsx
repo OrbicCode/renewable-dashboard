bb84fc47f5df3e3e0ba70920dea9fada727e882e
James O'Kane <orbiccode@gmail.com>
2025-11-27 16:49:47 +0000

    added test data to Line graph

--- frontend/src/components/HistoricalIntensityLine/HistoricalIntensityLine.tsx
+++ frontend/src/components/HistoricalIntensityLine/HistoricalIntensityLine.tsx
@@ -1,21 +1,43 @@
+import { Line } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
 import type { HistoricalIntensity } from '@shared/types';
 
 interface HistoricalIntensityLineProps {
   data: HistoricalIntensity[] | null;
 }
 
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+
 export default function HistoricalIntensityLine({ data }: HistoricalIntensityLineProps) {
+  const chartData = {
+    labels: data ? data.map((int) => int.actual) : [],
+    datasets: [
+      {
+        labels: data ? data.map((int) => int.actual) : [],
+        data: data ? data.map((int) => int.actual) : [],
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    title: {
+      display: true,
+    },
+  };
+
   return (
     <>
-      <h2>I'm a Line chart</h2>
-      {data &&
-        data.map((intensity: HistoricalIntensity) => {
-          return (
-            <div>
-              <p>{intensity.actual}</p>
-            </div>
-          );
-        })}
+      <Line data={chartData} options={options} />
     </>
   );
 }
