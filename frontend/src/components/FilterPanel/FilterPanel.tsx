0ab1e324a40f04eff650b3ad0306dab04b4b0195
James O'Kane <orbiccode@gmail.com>
2025-12-17 21:07:40 +0000

    added arrow close button for large screens

--- frontend/src/components/FilterPanel/FilterPanel.tsx
+++ frontend/src/components/FilterPanel/FilterPanel.tsx
@@ -1,5 +1,7 @@
 import styles from './FilterPanel.module.css';
 import { Close } from '@mui/icons-material';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import DateFilter from '../filters/DateFilter/DateFilter';
 
 interface FilterPanelProps {
   onFilterPanelClose: () => void;
@@ -17,10 +19,16 @@ export default function FilterPanel({
       <button className={styles.closeBtn} onClick={onFilterPanelClose}>
         <Close fontSize="large" />
       </button>
+      <button className={styles.arrowBtn} onClick={onFilterPanelClose}>
+        <ArrowForwardIcon fontSize="large" />
+      </button>
       {activeFilterPanel === 'gen-mix' ? (
         <h2>Gen-mix Filters</h2>
       ) : activeFilterPanel === 'hist-int-line' ? (
-        <h2>Historical Intensity Filters Filters</h2>
+        <>
+          <h2>Historical Intensity Filters Filters</h2>
+          <DateFilter />
+        </>
       ) : (
         <h2>Filters</h2>
       )}
