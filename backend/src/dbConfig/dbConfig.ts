10d4dae9227b4a012749b8e026fb24bda2b2e608
James O'Kane <orbiccode@gmail.com>
2025-12-25 18:11:05 +0000

    removed path in config

--- backend/src/dbConfig/dbConfig.ts
+++ backend/src/dbConfig/dbConfig.ts
@@ -1,9 +1,7 @@
 import { Pool } from 'pg';
 import dotenv from 'dotenv';
 
-dotenv.config({
-  path: 'backend/.env',
-});
+dotenv.config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
