b5737a5fb3a22c67b22587212575927cb9b426d5
James O'Kane <orbiccode@gmail.com>
2026-01-06 12:35:36 +0000

    init component + fetch logic

--- frontend/src/components/filters/HistoricalIntensityFilterPanel/HistoricalIntensityFilterPanel.tsx
+++ frontend/src/components/filters/HistoricalIntensityFilterPanel/HistoricalIntensityFilterPanel.tsx
@@ -1,5 +1,37 @@
+import { useState } from 'react';
 import DateFilter from '../DateFilter/DateFilter';
 
 export default function HistoricalIntensityFilterPanel() {
-  return <DateFilter />;
+  const [startDate, setStartDate] = useState<string>('');
+  const [endDate, setEndDate] = useState<string>('');
+
+  async function handleApplyFilters() {
+    try {
+      if (startDate || endDate) {
+        const response = await fetch('http://localhost:3000/historical/get-intensities', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ startDate, endDate }),
+        });
+        const data = await response.json();
+        console.log(data);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return (
+    <>
+      <DateFilter
+        setStartDate={setStartDate}
+        setEndDate={setEndDate}
+        startDate={startDate}
+        endDate={endDate}
+      />
+      <button onClick={handleApplyFilters}>Apply Filters</button>
+    </>
+  );
 }
